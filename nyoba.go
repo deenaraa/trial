fafab3450cebe536993ee047d05940365402cd59
deenaraa <104327208+deenaraa@users.noreply.github.com>
2024-09-11 22:11:06 +0800

    Add files via upload

---
+++ nyoba.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	test
+	*/
+	var FirstName string = "Dee"
+	var LastName string 
+	LastName = "Nara"
+
+	fmt.Printf("Hello, %s %s!\n", FirstName, LastName)
+
+}
+
+//test
